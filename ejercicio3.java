4a74a2226861cbf89017a502f3bc494a622a6cab
ricardo <dev.riclr@gmail.com>
2022-04-28 17:25:09 -0500

    cambios 1

---
+++ ejercicio3.java
@@ -0,0 +1,9 @@
+/**
+ * ejercicio3
+ */
+public class ejercicio3 {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ holaMundo3.java
@@ -0,0 +1,9 @@
+/**
+ * holaMundo3
+ */
+public class holaMundo3 {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+    }
+}
